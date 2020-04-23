e586ad8e5bc2e0947ce5c048797944c3dbc367be
Loic Coyle <loic.coyle@hotmail.fr>
2020-04-23 19:12:07 +0200

    v1.1.2

--- blm_header/__init__.py
+++ blm_header/__init__.py
@@ -4,6 +4,6 @@ from .header_maker import HeaderMaker
 
 logger = logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 __author__ = 'Loic Coyle <loic.coyle@hotmail.fr>'
 
